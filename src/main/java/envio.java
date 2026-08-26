1b397e63d232f55dc07d24e12521b253f6daec6f
Angel Gonzalez <a222213581@unison.mx>
2026-08-26 12:38:54 -0700

    Paso 3: TDD Green - Implementación mínima que hace pasar las pruebas

--- src/main/java/envio.java
+++ src/main/java/envio.java
@@ -1,5 +1,11 @@
 public class Envio {
     public static double calcularCostoEnvio(double importe) {
-        return -1; // valor incorrecto a propósito
+        if (importe < 500) {
+            return 100;
+        } else if (importe < 1000) {
+            return 50;
+        } else {
+            return 0;
+        }
     }
 }
